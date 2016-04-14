711993f087b574d7b9e82e5d32a8189e52ea1b5e
rukeon <rukeon@snu.ac.kr>
2016-04-14 17:47:15 +0900

    python practice

--- python_code_practice.py
+++ python_code_practice.py
@@ -4,3 +4,14 @@ keys = ['a', 'b', 'c']
 values = [1,2,3]
 
 hash = {k:v for k,v in zip(keys, values)}
+
+#2 make reducer
+def myreducer(fnc, seq):
+	tally = seq[0]
+	for n in seq[1:]:
+		tally = fnc(tally, n)
+	return tally
+
+myreducer( (lambda x, y: x*y), [1,2,3,4])
+
+
